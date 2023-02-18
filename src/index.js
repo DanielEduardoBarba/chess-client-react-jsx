27d50185fa9dec77025236c2342edd5c91b2a8c3
Daniel Barba <frdb103@gmail.com>
2023-02-18 16:40:21 -0500

    deployable-mvp

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <>
     <React.StrictMode>
+      <App  />
     </React.StrictMode>
-      <App className="body-main" />
   </>
 )
 
--- src/scenes/Welcome.jsx
+++ src/scenes/Welcome.jsx
@@ -5,7 +5,6 @@ export default function Welcome({setWelcome}){
     return(
         <>
         <div className="welcome-container">
-
         <h1 className="greeting">Call it Chess!</h1>
         <button className="call-to-action" onClick={()=>setWelcome(1)}>Click to PLAY!</button>
         </div>
